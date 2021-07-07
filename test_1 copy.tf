99fe2d4908de051d5871b7bbab8ad29174968114
sar21 <sa5347984@gmail.com>
2021-07-07 21:31:43 +0100

    changes made

--- test_1 copy.tf
+++ test_1 copy.tf
@@ -10,3 +10,40 @@ resource "aws_instance" "cloud1" {
     count = "2"
 }
 
+# Creation of S3 public and private buckets 
+resource "aws_s3_bucket" "cloud1" {
+  bucket = "motivateam4media"
+  acl    = "public-read"
+
+}
+
+
+resource "aws_s3_bucket" "cloud1_bkp" {
+  bucket = "cloud14bkp"
+  acl    = "private"
+}
+
+
+resource "aws_s3_bucket_policy" "cloud1" {
+  bucket = "cloud1media"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "cloud1_policy",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+    "Principal": "*",
+      "Action": [
+          "s3:GetObject"
+          ],
+      "Resource":[
+          "arn:aws:s3:::cloud1edia/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
